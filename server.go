eed0efe89eaa71b85e75ae8f9d951a346e1e250f
Keoni Murray <keonimurray45@gmail.com>
2019-02-05 15:40:31 -0800

    finished api challenge

--- server.go
+++ server.go
@@ -1,74 +1,70 @@
 package main
 
-
 import (
 	"encoding/json"
 	"io/ioutil"
-	"github.com/labstack/echo"
 	"net/http"
 	"fmt"
+
+	"github.com/labstack/echo"
 )
 
-
-
 //Joke represents a random bad joke sent
-
-
-type Joke []struct{
-	
-	ID string `json:"id"`
-	Type string `json:"type"`
-	Setup string `json:"setup"`
+type Joke struct {
+	ID        string `json:"id"`
+	Type      string `json:"type"`
+	Setup     string `json:"setup"`
 	Punchline string `json:"punchline"`
 }
 
-
-
-func main(){
+func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello Keoni!")
-	})
+	
 
-	e.GET("/joke", func(c echo.Context) error{
+	//call the api
 
-		//create request
-		req, err := http.NewRequest("GET", "https://official-joke-api.appspot.com/random_joke", nil)
+	response, err := http.Get("https://official-joke-api.appspot.com/random_joke")
 
-		// handle error
-		if err != nil{
-			fmt.Println("Error is req:", err)
-		}
+	fmt.Println("this is response", response)
+	fmt.Println("this is response err:", err)
 
-		// create a Client
-		client := &http.Client{}
 
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println("error in sending req", err)
-		}
 
-		// Response data is available in response.Body; read until error or EOF.
-		body, readErr := ioutil.ReadAll(resp.Body)
-		if readErr != nil {
-			
-			fmt.Println(readErr)
+	if err != nil{
+		fmt.Println("This is a req error", err)
+
 	}
-		//create a new joke and store data 
-		joke := &Joke{} 
-
-		var resObj req
-
-		parseErr := json.Unmarshal(body, &joke)
-
-		if parseErr != nil {
-			fmt.Println(parseErr)
-			
-		}
-		fmt.Println(joke)
-		return c.JSON(http.StatusOK, joke)
-	})
 
 	
-	e.Logger.Fatal(e.Start(":1323"))
+
+	// get the response
+	data, rederr := ioutil.ReadAll(response.Body)
+	fmt.Println("this is data ", data)
+	fmt.Println("this is rederr:", rederr)
+
+	if rederr != nil{
+		fmt.Println("This is a read error:", rederr)
+	}
+
+	// Instantiate new Joke
+
+	var joke Joke
+
+	fmt.Print("this is joke variable", joke)
+
+	// Unmarshal data
+
+	json.Unmarshal(data, &joke)
+
+	fmt.Println("this is unmarshaling json data",json.Unmarshal(data, &joke))
+
+	
+
+		return c.JSON(http.StatusOK, joke)
+
+	})
+
+	e.Logger.Fatal(e.Start(":4000"))
+
 }
